eb6038cf7e37669eebbd4c154536c1fd8f4f4d38
Jeremy Parker <jrobinparker@gmail.com>
2018-06-14 22:51:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reversed_array = []
   sentence_array.collect do |word|
-    word.reverse
+    word.reverse!
     reversed_array << word 
   end 
   puts reversed_array
