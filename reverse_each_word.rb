3442f3e21b13c8ab2bdfeb4fb8e88bf0651d7449
Jeremy Parker <jrobinparker@gmail.com>
2018-06-14 22:47:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(sentence)
-  split_sentence = sentence.to_a
   reversed_sentence = sentence.reverse
-  reversed_sentence = reversed_sentence.to_s
   puts reversed_sentence
 end
